bb5d0780bdca756f159a7b8462180e35896cfc81
felipelippert <felipe.v.lippert@gmail.com>
2023-07-17 23:54:01 -0300

    fix

--- main.js
+++ main.js
@@ -51,7 +51,7 @@ function atualizaMedia() {
     
     const mediaFinal = calculaMediaFinal()
 
-    document.getElementById('media-final-valor').innerHTML = mediaFinal
+    document.getElementById('media-final-valor').innerHTML = mediaFinal.toFixed(2)
     document.getElementById('media-final-resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado
 }
 
